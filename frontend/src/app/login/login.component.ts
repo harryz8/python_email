4a9aef90cef154203f27f85a8f797e89aa34d12a
Harry Zoeftig <harry.zoeftig@btinternet.com>
2025-07-26 17:23:18 +0100

    Started work on inbox front-end
    initial implementation

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -33,8 +33,6 @@ export class LoginComponent {
   }
 
   loginUser(response : any) : void {
-    alert(response)
-    alert(response['token'])
     localStorage.setItem('token', response['token']);
     this.userService.setCurrentUser(response['user'] as IUser);
   }
